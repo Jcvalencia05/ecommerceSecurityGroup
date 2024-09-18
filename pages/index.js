a8a2d753e276762d4671c1a816e8797c551b1a23
Jcvalencia05 <valencia.jc93@gmail.com>
2024-09-17 19:58:34 -0500

    third commit

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,9 @@
+import Layout from "@/components/Layout";
+import Nav from "@/components/Nav";
 import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Home() {
-  const { data: session } = useSession()
-  if(!session){
-    return (
-      <div className={'bg-blue-500 w-screen h-screen flex items-center'}>
-      <div className="text-center w-full">
-        <button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-md">Iniciar con Gmail</button>
-      </div>
-    </div>
-    )
-  }
-  return (
-    <div>Bienvenido {session.user.email}</div>
-  );
+  return <Layout>
+    prueba
+  </Layout>
 }
