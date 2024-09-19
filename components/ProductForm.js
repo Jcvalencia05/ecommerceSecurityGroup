5a868efafc020f228565c079efd5363696389a41
Jcvalencia05 <valencia.jc93@gmail.com>
2024-09-18 23:29:19 -0500

    delete product

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -1,8 +1,10 @@
+import { data } from "autoprefixer";
 import axios from "axios";
 import { useRouter } from "next/router";
 import { useState } from "react";
 
 export default function ProductForm({
+    _id,
     title:existingTitle,
     description:existingDescription,
     price:existingPrice,
@@ -12,17 +14,23 @@ export default function ProductForm({
     const [price, setPrice] = useState(existingPrice || '');
     const [goToProducts, setGoToProducts] = useState(false);
     const router = useRouter();
-    async function createProduct(ev) {
+    async function saveProduct(ev) {
         ev.preventDefault();
         const data= {title,description,price};
-        await axios.post('/api/products', data);
+        if(_id){
+            //uodate
+            await axios.put('/api/products', {...data,_id});
+        }else{
+            //create
+            await axios.post('/api/products', data);
+        }
         setGoToProducts(true);    
     }
     if(goToProducts){
         router.push('/products');
     }
     return (
-        <form onSubmit={createProduct}>
+        <form onSubmit={saveProduct}>
             <input type="text" placeholder="Nombre del producto" value={title}
                 onChange={ev => setTitle(ev.target.value)} />
             <textarea placeholder="Descripción del producto" value={description}
